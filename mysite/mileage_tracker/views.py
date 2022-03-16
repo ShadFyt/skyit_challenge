4418681c9f9295b64ac294060138821452cf4e22
ShadFyt <doanryan10@gmail.com>
2022-03-15 22:19:38 -0500

    refactor serializers.py

--- mysite/mileage_tracker/views.py
+++ mysite/mileage_tracker/views.py
@@ -1,14 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from mileage_tracker.models import Vehicle
 from mileage_tracker.serializers import VehicleSerializer
 
 # Create your views here.
-@csrf_exempt
+@api_view(["GET", "POST"])
 def vehicle_list(request):
     """
     List all vehicles in table or create new vehicle
@@ -16,18 +13,17 @@ def vehicle_list(request):
     if request.method == "GET":
         vehicle = Vehicle.objects.all()
         serializer = VehicleSerializer(vehicle, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = VehicleSerializer(data=data)
+        serializer = VehicleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(["GET", "PUT", "DELETE"])
 def vehicle_details(request, pk):
     """
     Get, Update or Delete a `Vehicle`
@@ -36,20 +32,19 @@ def vehicle_details(request, pk):
     try:
         vehicle = Vehicle.objects.get(pk=pk)
     except Vehicle.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
         serializer = VehicleSerializer(vehicle)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = VehicleSerializer(vehicle, data=data)
+        serializer = VehicleSerializer(vehicle, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
         vehicle.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
